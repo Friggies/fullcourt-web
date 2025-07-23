6ba901fcd1966463b879652773a9d24a29a04148
Friggies <85342562+Friggies@users.noreply.github.com>
2025-07-23 20:52:46 +0200

    feat: add animated tag image and update layout in Index component; enhance Tailwind configuration with new animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
-import { Hero } from "@/components/hero";
-import { Metadata } from "next";
+import { Hero } from '@/components/hero';
+import { Metadata } from 'next';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
-    title: "FULLCOURT TRAINING",
-    description: "",
+  title: 'FULLCOURT TRAINING',
+  description: '',
 };
 
 export default function Index() {
-    return (
-        <>
-            <Hero title="Animated basketball drills for&nbsp;the&nbsp;entire&nbsp;court" />
-        </>
-    );
+  return (
+    <>
+      <Image
+        src={'/animations/tag.webp'}
+        alt="Fun Tag Animation"
+        width={1080}
+        height={1080}
+        className="h-[150px] w-auto mx-auto mt-10 mb-5 animate-spin-slow"
+      />
+      <Hero title="Animated basketball drills for&nbsp;the&nbsp;entire&nbsp;court" />
+    </>
+  );
 }
