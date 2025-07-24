e1a537f69505ec5b2e4fe9538e0d04726917429b
Friggies <85342562+Friggies@users.noreply.github.com>
2025-07-24 15:08:02 +0200

    refactor: restructure Index component layout for improved organization and readability

--- app/page.tsx
+++ app/page.tsx
@@ -11,15 +11,27 @@ export const metadata: Metadata = {
 export default function Index() {
   return (
     <>
-      <Image
-        src={'/animations/passing.gif'}
-        alt="Fun Passing Animation"
-        unoptimized
-        priority
-        width={1080}
-        height={1080}
-        className="h-[150px] w-auto mx-auto mt-10 mb-5 animate-spin-slow"
-      />
+      <div>
+        <div className="relative h-[250px] w-[250px] mx-auto rounded-lg border shadow-sm overflow-hidden flex items-center justify-center">
+          <Image
+            src={'/images/floor.webp'}
+            alt="Floor Plan"
+            priority
+            width={1080}
+            height={1080}
+            className="absolute top-0 left-0 w-full h-full object-cover"
+          />
+          <Image
+            src={'/animations/passing.gif'}
+            alt="Fun Passing Animation"
+            unoptimized
+            priority
+            width={1080}
+            height={1080}
+            className="h-[150px] w-auto  animate-spin-slow"
+          />
+        </div>
+      </div>
       <Hero title="Animated basketball drills for&nbsp;the&nbsp;entire&nbsp;court" />
       <B2BDisclaimer />
     </>
