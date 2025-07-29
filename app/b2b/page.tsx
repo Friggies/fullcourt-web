f24b4da2d97967ca73f00100f93a1e2cccb86266
Frej Dahl Linneke <85342562+Friggies@users.noreply.github.com>
2025-07-29 20:52:56 +0200

    feat: update text content for clarity and engagement on B2B page

--- app/b2b/page.tsx
+++ app/b2b/page.tsx
@@ -14,7 +14,7 @@ export default function B2B() {
               Clubs & organizations
             </span>
             <span className="font-semibold text-xl lg:text-2xl">
-              Working with teams you know
+              Working with teams who want to take their game to the next level
             </span>
           </h2>
           <p>
@@ -22,13 +22,6 @@ export default function B2B() {
             iste, nulla fuga ad, voluptate atque quibusdam recusandae facilis
             error eos voluptates, rerum est ex accusamus quos sit hic! Aperiam.
           </p>
-          <Link
-            href="/drills"
-            className="flex items-center gap-2 bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition-colors duration-300"
-          >
-            Pass us an email
-            <MailIcon size={16} strokeWidth={1.5} />
-          </Link>
         </div>
         <Image
           className="aspect-[9/16] w-full sm:w-auto sm:h-[400px] border rounded-lg shadow-sm"
@@ -45,7 +38,7 @@ export default function B2B() {
               Custom animations
             </span>
             <span className="font-semibold text-xl lg:text-2xl">
-              Help your team with custom animations
+              Help your team with custom animations of your own drills and plays
             </span>
           </h2>
           <p>
@@ -57,7 +50,7 @@ export default function B2B() {
             href="mailto:contact@fullcourt-training.com"
             className="flex items-center gap-2 bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition-colors duration-300"
           >
-            Pass us an email
+            Contact
             <MailIcon size={16} strokeWidth={1.5} />
           </Link>
         </div>
