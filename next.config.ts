5b7e8c46a136ced3f5ce1ff05668abb873ecf41d
Frej Dahl Linneke <85342562+Friggies@users.noreply.github.com>
2026-02-27 17:00:13 +0100

    added skew protection

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  deploymentId: process.env.DEPLOY_ID, //for Netlify skew protection
   serverExternalPackages: ['sharp'],
   images: { domains: ['crbswbfgtbkjinzagblg.supabase.co'] },
 };
