2b96e75b00d643c322922e3a0f55d1f200b105a7
Frej Dahl Linneke <85342562+Friggies@users.noreply.github.com>
2026-02-27 17:04:26 +0100

    migrated from domain to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   deploymentId: process.env.DEPLOY_ID, //for Netlify skew protection
   serverExternalPackages: ['sharp'],
-  images: { domains: ['crbswbfgtbkjinzagblg.supabase.co'] },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'crbswbfgtbkjinzagblg.supabase.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
