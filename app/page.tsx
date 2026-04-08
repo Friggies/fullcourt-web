020f348eac9df8e6e818136bdf69b15b2af3c5eb
Frej Dahl Linneke <85342562+Friggies@users.noreply.github.com>
2026-04-08 08:45:39 +0200

    changed wording

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,7 @@ export default function Index() {
 
       <Section className="bg-muted py-5 text-muted-foreground mb-5">
         <div className="flex flex-wrap justify-center items-center gap-x-1 gap-y-1">
-          <span className="whitespace-nowrap">Used by</span>
+          <span className="whitespace-nowrap">Followed by</span>
 
           <span className="flex -space-x-2" aria-label="User headshots">
             {Headshots.map((src, i) => (
